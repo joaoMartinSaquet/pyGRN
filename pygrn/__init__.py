464bfd7f6484ac2884b6a5bb36cd8607075f9453
MOI <jmartinsaquet@lrtechnologies.fr>
2025-07-17 14:28:25 +0200

    tensorflow residual removal and regression working

--- pygrn/__init__.py
+++
@@ -1,1 +0,0 @@
-from .recurrent import RGRN
--- pygrn/grns/classic.py
+++ pygrn/grns/classic.py
@@ -1,17 +1,21 @@
 import numpy as np
 from copy import deepcopy
 from .base import GRN
-
+from loguru import logger
 
 class ClassicGRN(GRN):
     """Classic CPU-based GRN
 
     Dynamics equations are written mostly in loop form
+    input index is 0:n_inputs
+    output id is n_inputs : n_output 
     """
     concentration = []
     next_concentration = []
     enhance_match = []
     inhibit_match = []
+    
+
     
     def __init__(self):
         pass
@@ -63,34 +67,48 @@ class ClassicGRN(GRN):
         return self.concentration[self.num_input:(
             self.num_output + self.num_input)]
 
-    def step(self):
-        if len(self.next_concentration) != len(self.concentration):
-            self.next_concentration = np.zeros(len(self.concentration))
-        sum_concentration = 0.0
-        for k in range(len(self.identifiers)):
-            if k < self.num_input:
-                self.next_concentration[k] = self.concentration[k]
-            else:
-                enhance = 0.0
-                inhibit = 0.0
-                for j in range(len(self.identifiers)):
-                    if not (j >= self.num_input and
-                            j < (self.num_output + self.num_input)):
-                        enhance += (self.concentration[j] *
-                                    self.enhance_match[j, k])
-                        inhibit += (self.concentration[j] *
-                                    self.inhibit_match[j, k])
-                diff = self.delta / len(self.identifiers) * (enhance - inhibit)
-                self.next_concentration[k] = max(0.0,
-                                                 self.concentration[k] + diff)
-                sum_concentration += self.next_concentration[k]
-        if sum_concentration > 0:
-            for k in range(len(self.identifiers)):
-                if k >= self.num_input:
-                    self.next_concentration[k] = min(
-                        1.0, self.next_concentration[k] / sum_concentration)
+    def step(self, nsteps=1):
+        """Runs the GRN concentration update"""
+        
 
-        self.concentration = self.next_concentration
+
+        for step in range(nsteps):
+            if len(self.next_concentration) != len(self.concentration):
+                self.next_concentration = np.zeros(len(self.concentration))
+            
+            sum_concentration = 0.0
+            for k in range(len(self.identifiers)):
+
+                # if the proteins is an input proteins she is not impacted by the network
+                if k < self.num_input:
+                    self.next_concentration[k] = self.concentration[k]
+                else:
+                    # we compute the concentration change 
+                    enhance = 0.0
+                    inhibit = 0.0
+                    for j in range(len(self.identifiers)):
+                        # if the proteins is not an ouptut  because it does not regulate the network we compute hk and gk for all execpt the output 
+                        if j < self.num_input or j >= (self.num_output + self.num_input):
+                            enhance += (self.concentration[j] *
+                                        self.enhance_match[j, k])
+                            inhibit += (self.concentration[j] *
+                                        self.inhibit_match[j, k])
+                                      
+                    diff = self.delta  * (enhance - inhibit) / len(self.identifiers) 
+                    # logger.debug("enhance: {}, inhibit: {}, diff: {}".format(enhance, inhibit, diff))
+                    # logger.debug("enhance match: {}".format(self.enhance_match))
+                    
+                    self.next_concentration[k] = max(0.0,
+                                                    self.concentration[k] + diff)
+                    sum_concentration += self.next_concentration[k]
+            if sum_concentration > 0:
+                for k in range(len(self.identifiers)):
+                    if k >= self.num_input:
+                        self.next_concentration[k] = min(
+                            1.0, self.next_concentration[k] / sum_concentration)
+
+            # logger.debug("Concentration: {},  \t next_concentration: {}".format(self.concentration, self.next_concentration))
+            self.concentration = self.next_concentration
         return self
 
     def clone(self):
